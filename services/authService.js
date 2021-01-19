3cb78dabd6b328c5332f04d3372bf1f00079a73c
Sergey <sergey_chizhik@ukr.net>
2021-01-19 22:42:03 +0200

    Update authService.js

--- services/authService.js
+++ services/authService.js
@@ -1,10 +1,21 @@
+const throwCustomError = require('../helpers/CustomError');
 const UserService = require('./userService');
+const errors = require('../constants/statusCodes');
 
 class AuthService {
+    singUp(userData) {
+        const user = UserService.createNewUser(userData);
+        
+        if(!user) {
+            throw throwCustomError('Could not create a new user', errors.BAD_REQUEST);
+        }
+        return user;
+    }
+
     login(userData) {
         const user = UserService.search(userData);
         if(!user) {
-            throw Error('User not found');
+            throwCustomError('User not found', errors.NOT_FOUND);
         }
         return user;
     }
