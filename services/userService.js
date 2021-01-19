6fff200c3a2a5a415c7950a60f751a5b47e65f80
Sergey <sergey_chizhik@ukr.net>
2021-01-19 22:42:06 +0200

    Update userService.js

--- services/userService.js
+++ services/userService.js
@@ -3,6 +3,21 @@ const { UserRepository } = require('../repositories/userRepository');
 class UserService {
 
     // TODO: Implement methods to work with user
+    getUserById(id) {
+        const user = UserRepository.getOne(search);
+        if(!user) {
+            return null;
+        }
+        return item;
+    }
+
+    createNewUser(userData) {
+        const item = UserRepository.create(userData);
+        if (!item) {
+            return null;
+        }
+        return item;
+    }
 
     search(search) {
         const item = UserRepository.getOne(search);
