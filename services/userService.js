bc67d45f691dfc5b1093e301aaef8991fb548b5c
Sergey <sergey_chizhik@ukr.net>
2021-01-19 22:59:05 +0200

    Update userService.js

--- services/userService.js
+++ services/userService.js
@@ -1,22 +1,60 @@
+const throwCustomError = require('../helpers/CustomError');
 const { UserRepository } = require('../repositories/userRepository');
+const errors = require('../constants/statusCodes');
 
 class UserService {
 
-    // TODO: Implement methods to work with user
-    getUserById(id) {
-        const user = UserRepository.getOne(search);
-        if(!user) {
-            return null;
+    getUsers() {
+        const users = UserRepository.getUsers();
+
+        if(!users) {
+            throwCustomError('No users.', errors.NOT_FOUND);
         }
-        return item;
+        return users;
+    }
+    
+    getUserById(id) {
+        const user = UserRepository.getUserById(id);
+        if(!user) {
+            throwCustomError(`User with id ${id} not found.`, errors.NOT_FOUND);
+        }
+        return user;
     }
 
-    createNewUser(userData) {
-        const item = UserRepository.create(userData);
-        if (!item) {
-            return null;
+    createUser(userData) {
+        const user = UserRepository.create(userData);
+        if (!user) {
+            throwCustomError(`Could not create a new user.`, errors.BAD_REQUEST);
         }
-        return item;
+        return user;
+    }
+
+    updateUser(id, user) {
+        const oldUser = UserRepository.getUserById(id);
+        if(!oldUser) {
+            throwCustomError(`User with id ${id} not found.`, errors.NOT_FOUND);
+        }
+
+        oldUser.firstName = user.firstName;
+        oldUser.lastName = user.lastName;
+        oldUser.email = user.email;
+        oldUser.phoneNumber = user.phoneNumber;
+
+        const updatedUser = UserRepository.updateUser(oldUser);
+
+        if (!updatedUser) {
+            throwCustomError(`Could not update a old user with id ${id}.`, errors.BAD_REQUEST);
+        }
+
+        return user;
+    }
+
+    deleteUserById(id) {
+        const result = UserRepository.deleteUserById(id);
+        if(!result) {
+            throwCustomError(`Could not delete a user with id ${id}.`, errors.BAD_REQUEST);
+        }
+        return result;
     }
 
     search(search) {
