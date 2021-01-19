826263a5d2f341e1bdbf09d47e543c766c9039b9
Sergey <sergey_chizhik@ukr.net>
2021-01-19 22:58:54 +0200

    Update response.middleware.js

--- middlewares/response.middleware.js
+++ middlewares/response.middleware.js
@@ -1,5 +1,4 @@
 const responseMiddleware = (req, res, next) => {
-   // TODO: Implement middleware that returns result of the query
     if (res.err != undefined) {
         const {status, message, error} = res.err;
         res.status(status).send({message, error});
