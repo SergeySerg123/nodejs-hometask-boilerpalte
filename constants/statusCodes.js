f1a05053a7470ce49f125982fa979cfe129acd82
Sergey <sergey_chizhik@ukr.net>
2021-01-22 15:57:19 +0200

    Update statusCodes.js

--- constants/statusCodes.js
+++ constants/statusCodes.js
@@ -1,11 +1,9 @@
 const NOT_FOUND = 404;
 const BAD_REQUEST = 400;
 const UNAUTHORIZED = 401;
-const NO_CONTENT = 204;
 
 module.exports = {
     NOT_FOUND,
     BAD_REQUEST,
     UNAUTHORIZED,
-    NO_CONTENT
 }
